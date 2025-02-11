57aa244e17a2e030c4a729369aabaf1cf355dfa1
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-02-11 11:34:37 -0600

    POR FIN LIBRES DE BUGS Y YA MUESTRA LA TABLA

--- src/users/presentation/render-table/render-table-.js
+++ src/users/presentation/render-table/render-table-.js
@@ -1,38 +1,34 @@
-import  './render-table.css'
+import usersStore from "../../store/users-store";
+import './render-table.css';
 
 let table;
 
- const createTable = () => {
-     const table = document.createElement('table');
-     const tableHead = document.createElement('thead');
-     tableHead.innerHTML = `
-      <tr>
-        <th>ID</th><
-        <th>Balance</th><
-        <th>First Name</th><
-        <th>Last Name</th><
-        <th>Active</th><
-        <th>Actions</th><
-      </tr>
-     
-     `;
+const createTable = () => {
+    const table = document.createElement('table');
+    const tableHeaders = document.createElement('thead');
+    tableHeaders.innerHTML = `
+        <tr>
+            <th>ID</th>
+            <th>Balance</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Active</th>
+            <th>Actions</th>
+        </tr>
+    `;
 
-     const tableBody = document.createElement('tbody');
-     tableBody.append(tableHead, tableBody);
-     return table;
-     
- }
+    const tableBody = document.createElement('tbody');
+    table.append(tableHeaders, tableBody); // Agrega los elementos a la tabla
+    return table;
+};
 
 export const renderTable = (element) => {
-
-    const users = usersStore.getUser();
+    const users = usersStore.getUsers();
 
     if (!table) {
-        table = createTable(); 
+        table = createTable();
         element.append(table);
         // TODO: listener a la tabla
     }
-
-
-}
+};
     
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,9 +1,14 @@
+import { renderTable } from "./presentation/render-table/render-table-";
 import usersStore from "./store/users-store";
 
 
 
 export const UserAppp = async (element) => {
+
     element.innerHTML = 'Loading...';
     await usersStore.loandNextPage();
+    element.innerHTML = '';
+    
+    renderTable(element);
 
 };
