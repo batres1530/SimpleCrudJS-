3839ad29c72e7cfc9196ef1810ee80e13e42c282
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-02-12 11:52:47 -0600

    se agregó un modal para la entrada de usuario con su respectivo estilo y funcionalidad

---
+++ src/users/presentation/render-modal/render-modal.js
@@ -0,0 +1,13 @@
+import modalHtml from  './render-modal.html?raw';
+import './render-modal.css';
+let modal;
+
+export const  renderModal = (element) => {
+    if (modal)  return;
+
+    modal = document.createElement('div');
+    modal.innerHTML = modalHtml;
+    modal.className= 'modal-cantainer hide-modal';
+    element.append(modal);
+
+}
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -2,6 +2,8 @@ import { renderTable } from "./presentation/render-table/render-table-";
 import usersStore from "./store/users-store";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
 import {renderAddButton} from './presentation/render-add-button/render-add-button' 
+import {renderModal} from './presentation/render-modal/render-modal'
+
 
 
 
@@ -14,5 +16,6 @@ export const UserAppp = async (element) => {
     renderTable(element);
     renderButtons(element);
     renderAddButton(element);
-
+    renderModal(element);
+    
 };
