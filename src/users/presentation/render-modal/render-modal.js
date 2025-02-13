c354800092269fef6e353931e52f37290d3ba919
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-02-13 10:41:19 -0600

    se mejoró el estilo del modal y se implementaron funciones para mostrar y ocultar el modal

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -2,6 +2,15 @@ import modalHtml from  './render-modal.html?raw';
 import './render-modal.css';
 let modal;
 
+//todo: cargar el usario por id
+export const showModal = ()=>{
+    modlal?.classList.remove('hide-modal');
+}
+
+export const hideModal = ()=>{
+    modal?.classList.add('hide-modal');
+}
+
 export const  renderModal = (element) => {
     if (modal)  return;
 
